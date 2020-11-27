dc95416d9a45555463415573aadfab09726ad5ca
鲁伟明 <bthqh2yxtj@privaterelay.appleid.com>
2020-11-27 20:37:35 +0800

    新增二叉树VO

---
+++ src/main/java/com/smile/vo/BinaryTree.java
@@ -0,0 +1,15 @@
+package com.smile.vo;
+
+/**
+ * @author luweiming
+ * @date 2020/11/21 9:45 下午
+ */
+public class BinaryTree {
+
+    public int val;
+
+    public BinaryTree left;
+
+    public BinaryTree right;
+
+}
